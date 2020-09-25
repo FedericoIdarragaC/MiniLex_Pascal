3df51c0ca73416e76bb3db6ea585e734c36b5eb9
SebastianMoraC <sebastian.mora@utp.edu.co>
2020-09-25 10:19:42 -0500

    Cambio del txt a pas

--- MiniPascal.py
+++ MiniPascal.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
 	if (len(sys.argv) > 1):
 		fin = sys.argv[1]
 	else:
-		fin = 'evaluacion.txt'
+		fin = 'evaluacion.pas'
 	f = open(fin, 'r')
 	data = f.read()
 	print (data)
